8d4b56ab2a72cfbe4193bb407427a00a24220429
Shayne Boyer <spboyer@live.com>
2017-05-20 21:43:15 -0400

    Update Lab7 Code

--- Labs/Code/Lab7/Startup.cs
+++ Labs/Code/Lab7/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Lab7
+namespace Angular2Spa
 {
     public class Startup
     {
