12dc78bb9b247d7ce342f3de2bf5fb2113bf6bfb
Daniel Roth <daroth@microsoft.com>
2017-05-20 21:36:22 -0400

    Update Lab8

--- Labs/Code/Lab8/src/Lab8/Controllers/ProductsController.cs
+++ Labs/Code/Lab8/src/Lab8/Controllers/ProductsController.cs
@@ -1,15 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Lab8.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Lab8.Controllers
 {
-    [Route("/api/[controller]")]
     [Produces("application/json")]
-    public class ProductsController : ControllerBase
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
     {
         private static List<Product> _products = new List<Product>(new[] {
             new Product() { Id = 1, Name = "Computer" },
@@ -17,14 +19,17 @@ namespace Lab8.Controllers
             new Product() { Id = 3, Name = "Apple" },
         });
 
+        // GET: api/values
         [HttpGet]
-        public IEnumerable<Product> Get() => _products;
+        public IEnumerable<Product> Get()
+        {
+            return _products;
+        }
 
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
             var product = _products.SingleOrDefault(p => p.Id == id);
-
             if (product == null)
             {
                 return NotFound();
@@ -34,13 +39,12 @@ namespace Lab8.Controllers
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody]Product product)
+        public IActionResult Post([FromBody] Product product)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-
             _products.Add(product);
             return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
         }
